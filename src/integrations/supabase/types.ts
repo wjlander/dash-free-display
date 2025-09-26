02ce93503596407d69b4bf1c07e6f7a1fc63c85b
wjlander <will@w-j-lander.uk>
2025-09-26 08:24:39 +0100

    Updated types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -170,6 +170,42 @@ export type Database = {
         }
         Relationships: []
       }
+      google_calendar_configs: {
+        Row: {
+          access_token: string
+          created_at: string
+          expires_at: string
+          id: string
+          is_connected: boolean | null
+          refresh_token: string
+          scope: string
+          updated_at: string
+          user_id: string
+        }
+        Insert: {
+          access_token: string
+          created_at?: string
+          expires_at: string
+          id?: string
+          is_connected?: boolean | null
+          refresh_token: string
+          scope?: string
+          updated_at?: string
+          user_id: string
+        }
+        Update: {
+          access_token?: string
+          created_at?: string
+          expires_at?: string
+          id?: string
+          is_connected?: boolean | null
+          refresh_token?: string
+          scope?: string
+          updated_at?: string
+          user_id?: string
+        }
+        Relationships: []
+      }
       home_assistant_configs: {
         Row: {
           access_token: string
